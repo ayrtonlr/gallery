a6cc0174e390f3a9d0747a57397e06ca115ec02d
ayrtonlr <ayrtonlimarosa@gmail.com>
2021-03-03 18:40:37 -0300

    another commit

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+$(document).ready(function() {
+  $(".title").lettering();
+  $(".button").lettering();
+});
+
+$(document).ready(function() {
+  animation();
+}, 1000);
+
+$('.button').click(function() {
+  animation();
+});
+
+function animation() {
+  var title1 = new TimelineMax();
+  title1.to(".button", 0, {visibility: 'hidden', opacity: 0})
+  title1.staggerFromTo(".title span", 0.5, 
+  {ease: Back.easeOut.config(1.7), opacity: 0, bottom: -80},
+  {ease: Back.easeOut.config(1.7), opacity: 1, bottom: 0}, 0.15);
+  title1.to(".button", 0.5, {visibility: 'visible' ,opacity: 1})
+}
+
+
 let sliderIndex = 1;
 let timeout;
 const layers = [...document.querySelectorAll('.layer')];
@@ -39,3 +62,5 @@ function change(index) {
   document.getElementById("pic").src = imageLookup[index];
   document.getElementById("pic2").src = imageLookup2[index];
 }
+
+
